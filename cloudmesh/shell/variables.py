b083611c3f9af3d01e28ae4cc8970deb591ebd6c
Badi Abdul-Wahid <abdulwahidc@gmail.com>
2017-05-30 12:33:56 -0400

    fix:dev: use yaml to store variables

--- cloudmesh/shell/variables.py
+++ cloudmesh/shell/variables.py
@@ -1,17 +1,78 @@
 from __future__ import print_function
 
-import shelve
+from ruamel import yaml
+import os
+import os.path
 
 from cloudmesh.common.util import path_expand
 
 
+class VarDB(object):
+    """A replacement for :mod:`shelve` that should work for Python 2 and 3.
+
+    This uses a YAML file as the underlying persistent storage
+
+    Assumption
+    ==========
+
+    Values are strings so there is no need to pickle/unpickle objects.
+    """
+
+    def __init__(self, path):
+        self._db = dict()
+
+        self.path = path
+
+        prefix = os.path.dirname(self.path)
+        if not os.path.exists(prefix):
+            os.makedirs(prefix)
+
+        if os.path.exists(self.path):
+            with open(self.path) as dbfile:
+                self._db = yaml.safe_load(dbfile) or dict()
+
+        self.flush()
+
+    def flush(self):
+        with open(self.path, 'w') as dbfile:
+            yaml.dump(self._db, dbfile, default_flow_style=False)
+
+    def __setitem__(self, k, v):
+        assert isinstance(v, str) or isinstance(v, unicode), repr(v)
+        # the assertion should short-circuit, supporting Py2 and Py3
+        self._db[k] = v
+        self.flush()
+
+    def __getitem__(self, k):
+        return self._db[k]
+
+    def __delitem__(self, k):
+        del self._db[k]
+        self.flush()
+
+    def __contains__(self, k):
+        return k in self._db
+
+    def __iter__(self):
+        return iter(self._db)
+
+    def __len__(self):
+        return len(self._db)
+
+    def close(self):
+        "This is a NoOP for backwards compatibility"
+        pass
+
+    def clear(self):
+        "Truncate the database"
+        self._db.clear()
+        self.flush()
+
+
 class Variables(object):
     def __init__(self, filename=None):
-        if filename is None:
-            self.filename = path_expand("~/.cloudmesh/var-data")
-
-        self.data = shelve.open(self.filename, writeback=True)
-        self.data.sync()
+        self.filename = path_expand(filename or "~/.cloudmesh/var-data")
+        self.data = VarDB(self.filename)
 
     def __getitem__(self, key):
         if key not in self.data:
@@ -22,11 +83,9 @@ class Variables(object):
     def __setitem__(self, key, value):
         print ("set", key, value)
         self.data[str(key)] = value
-        self.data.sync()
 
     def __delitem__(self, key):
         del self.data[str(key)]
-        self.data.sync()
 
     def __contains__(self, item):
         return str(item) in self.data
@@ -46,10 +105,13 @@ class Variables(object):
             del self.data[key]
 
     def __iter__(self):
-        return self.data
+        return iter(self.data)
 
     def close(self):
         self.data.close()
+
+    def clear(self):
+        self.data.clear()
 
 
 if __name__ == "__main__":
