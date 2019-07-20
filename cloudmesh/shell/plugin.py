6aa3723edb045abf3ef0cc6aabc9efeecf561ff4
Gregor von Laszewski <laszewski@gmail.com>
2019-07-20 01:32:10 -0400

    comment on deprecation

--- cloudmesh/shell/plugin.py
+++ cloudmesh/shell/plugin.py
@@ -3,7 +3,6 @@ import yaml
 import requests
 import os
 
-
 class PluginManager(object):
 
     def __init(self):
@@ -11,6 +10,8 @@ class PluginManager(object):
 
     def load(self):
         """
+        TODO: DEPRECATED:
+
         loads the plugins form the plugin.yml file in github
 
         :return:
@@ -23,14 +24,30 @@ class PluginManager(object):
             entry['description'] = entry['description'].strip()
 
     def uninstall(self, name):
+        """
+        pip uninstalls the package with the specified name
+        :param name: The package name
+        :return:
+        """
         plugin = "cloudmesh." + name
         print("Uninstalling:", )
         os.system("pip uninstall {name}".format(name=plugin))
 
     def pip_install(self, name):
+        """
+        pip installs the package with the specified name
+        :param name: The package name
+        :return:
+        """
         plugin = "cloudmesh." + name
         print("Installing:", )
         os.system("pip install {name}".format(name=plugin))
 
     def source_install(self, name):
+        """
+        TODO: DEPRECATED
+
+        :param name:
+        :return:
+        """
         pass
