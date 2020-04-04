0bd46202e4c9f0970f89ccef9c1d320b16c87283
Gregor von Laszewski <laszewski@gmail.com>
2020-04-04 18:46:40 -0400

    safety push

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# Changelog
+
+
+## 4.2.22
+
+#### Enhancements and Changes:
+
+- move some of the tests that belong into cmd5 from other repos
+
+#### Bug Fixes:
+
+
--- tests/test_cms.py
+++ tests/test_cms.py
@@ -4,19 +4,21 @@
 # pytest -v --capture=no  tests/test_cms..py::Test_cms::<METHODNAME>
 ###############################################################
 import pytest
-from cloudmesh.common.debug import VERBOSE
+from cloudmesh.common.Benchmark import Benchmark
 from cloudmesh.common.Shell import Shell
+from cloudmesh.common.debug import VERBOSE
 from cloudmesh.common.util import HEADING
 
 
 @pytest.mark.incremental
 class TestConfig:
 
+
     def test_help(self):
         HEADING()
-
+        Benchmark.Start()
         result = Shell.execute("cms help", shell=True)
-
+        Benchmark.Stop()
         VERBOSE(result)
 
         assert "quit" in result
@@ -24,18 +26,22 @@ class TestConfig:
 
     def test_invalid_command(self):
         HEADING()
-
+        Benchmark.Start()
         result = Shell.execute("cms help wrong", shell=True)
-
+        Benchmark.Stop()
         VERBOSE(result)
 
         assert "No help on wrong" in result
 
     def test_version(self):
         HEADING()
-
+        Benchmark.Start()
         result = Shell.execute("cms version", shell=True)
-
+        Benchmark.Stop()
         VERBOSE(result)
 
-#        assert "No help on wrong" in result
+        # assert "No help on wrong" in result
+
+    def test_benchmark(self):
+        HEADING()
+        Benchmark.print(csv=True, sysinfo=False, tag="cmd5")
