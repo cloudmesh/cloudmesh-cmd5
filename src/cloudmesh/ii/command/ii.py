c1dd35543661ecace622451f1a3e1b7c0bf58536
Gregor von Laszewski <laszewski@gmail.com>
2023-12-17 06:28:03 -0500

    add inits in the subclasses

--- src/cloudmesh/ii/command/ii.py
+++ src/cloudmesh/ii/command/ii.py
@@ -13,29 +13,33 @@ class IiCommand(PluginCommand):
 
         """  # noqa: W605
 
+
         print ("ii")
-        from cloudmesh.cmd5.CloudmeshPlugin import  CloudmeshPlugin
-        print (CloudmeshPlugin.inheritors(PluginCommand))
-
-        print ("ho")
-        import cloudmesh
-        print (dir(cloudmesh))
-
-        print ("111", PluginCommand.__subclasses__())
-
-        import inspect
-        import cloudmesh
-        print(inspect.getmembers(PluginCommand, inspect.isclass))
-
-
-        for name in cloudmesh.__dict__:
-            print(name)
-        from pprint import pprint
-        pprint(globals())
-
+        # from cloudmesh.cmd5.CloudmeshPlugin import  CloudmeshPlugin
+        # print (CloudmeshPlugin.inheritors(PluginCommand))
+        #
+        # print ("ho")
+        # import cloudmesh
+        # print (dir(cloudmesh))
+        #
+        # print ("111", PluginCommand.__subclasses__())
+        #
+        # import inspect
+        # import cloudmesh
+        # print(inspect.getmembers(PluginCommand, inspect.isclass))
+        #
+        #
+        # for name in cloudmesh.__dict__:
+        #     print(name)
+        # from pprint import pprint
+        # pprint(globals())
+        #
         from cloudmesh.shell.shell import CMShell
+        #
+        # commands = dir(CMShell)
+        # commands = [s for s in commands if s.startswith("do_")]
+        # commands = [s.replace("do_", "") for s in commands]
 
-        commands = dir(CMShell)
-        commands = [s for s in commands if s.startswith("do_")]
-        commands = [s.replace("do_", "") for s in commands]
-        print (commands)
+        # print (commands)
+        import cloudmesh
+        print(dir(cloudmesh))
